3eec29cf0973b3cae8599a33b48871abbb13bc91
Stepanov Andrii <stepanovandriimail@gmail.com>
2025-07-23 00:41:10 +0300

    chore: configure routing for nutrition calculator

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from "@angular/router";
+import { NutritionCalculatorComponent } from "./features/nutrition-calculator/nutrition-calculator.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: "",
+        redirectTo: "nutrition-calculator",
+        pathMatch: "full"
+    },
+    {
+        path: "nutrition-calculator",
+        component: NutritionCalculatorComponent
+    }
+];
